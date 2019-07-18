5def1335e6930b31aa47743a4c893d36ef9c2bee
hanshi <18518759881@163.com>
2019-07-18 18:14:06 +0800

    7.18
    7.18

---
+++ utils/http.js
@@ -0,0 +1,35 @@
+import {config} from '../config.js'
+class HTTP {
+	constructor() {
+		this.baseRestUrl = config.api_blink_url
+	}
+	requst(params){
+		var that = this,
+			url = this.baseRestUrl + params.url;
+		if(!params.method){
+			params.methods = 'GET';
+		}
+		wx.request({
+			url:url,
+			data:params.data,
+			method:params.method,
+			header:{
+				'content-type':'application/json',
+				'appkey':config.appkey
+			},
+			sucess:function(res){
+				var code = res.statusCode.toString(),
+					startChar = code.charAt(0);
+				if(startChar == '2'){
+					params.success && params.success(res.data);
+				}else{
+					params.error && params.error(res) 
+				}
+			},
+			fail:function(err){
+				params.fail && params.fail(err)
+			}
+		});
+	}
+};
+export{HTTP};
